d98132b021faad224cf099345f5acb62a0488f90
Frank Kelly <frankintech00@gmail.com>
2023-09-06 15:42:38 +0100

    moved auth pages into auth folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
-import SignIn from "./pages/SignIn";
-import Test from "./pages/Test";
-import SignUp from "./pages/SignUp";
+import SignIn from "./pages/auth/SignIn";
+
+import SignUp from "./pages/auth/SignUp";
 
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
-      <h1>App</h1>
-      <Test />
       <SignIn />
       <SignUp />
     </AuthProvider>
--- src/pages/auth/SignIn.jsx
+++ src/pages/auth/SignIn.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { AuthContext } from "../context/AuthContext";
+import { AuthContext } from "../../context/AuthContext";
 
 function SignIn() {
   const [email, setEmail] = useState("");
