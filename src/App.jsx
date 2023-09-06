dee1676ce8eb78263e124cc4f2e10586279a986c
Frank Kelly <frankintech00@gmail.com>
2023-09-06 15:27:52 +0100

    added Test.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+// import SignUp from "./pages/SignUp";
+import Test from "./pages/Test";
 import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
     <AuthProvider>
       <h1>App</h1>
-      <SignUp />
+      <Test />
+      {/* <SignUp /> */}
       <SignIn />
     </AuthProvider>
   );
